b44d149f64b65afd17a9943e2a7b6a332353d9a3
Diogo Justen <diogojusten@gmail.com>
2017-11-15 17:46:11 -0200

    Get list of wav files

--- src/mp3_encoder.c
+++ src/mp3_encoder.c
@@ -17,16 +17,22 @@
 
 // Includes ///////////////////////////////////////////////////////////////////
 
+#include <dirent.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "mp3_encoder.h"
-
+#include <string.h>
+#include <glib.h>
 #include <lame/lame.h>
+#include "mp3_encoder.h"
 
 // Definitions ////////////////////////////////////////////////////////////////
 
+#define DEBUG
+
 #define LOG_IDENTIFICATION      "[MP3-ENCODER]"
 #define LOG_FACILITY            LOG_LOCAL1
+#define WAV_EXTENSION           ".wav"
+#define EXTENSION_SIZE          4
 
 // Macros /////////////////////////////////////////////////////////////////////
 
@@ -38,17 +44,84 @@
 
 // Private Variables //////////////////////////////////////////////////////////
 
+static GList *filename_list = NULL;
+
 // Private Functions //////////////////////////////////////////////////////////
+
+static int check_wav_extension(char *filename)
+{
+  int len;
+  char *p;
+  char extension[EXTENSION_SIZE];
+  int i;
+
+  g_assert(filename);
+
+  p = strrchr(filename, '.');
+  if (!p || p == filename) {
+    return -1;
+  }
+
+  if (strlen(p) != EXTENSION_SIZE) {
+    return -1;
+  }
+
+  for (i = 0; i < EXTENSION_SIZE; i++) {
+    if (p[i] != WAV_EXTENSION[i]) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+static int get_wav_list(char *folder_name)
+{
+  struct dirent **namelist;
+  int num_files;
+  int i;
+
+  g_assert(folder_name);
+
+  num_files = scandir(folder_name, &namelist, NULL, alphasort);
+  if (num_files < 0) {
+    printf("Error scandir: %s\n", folder_name);
+    return -1;
+  }
+
+  for (i = 0; i < num_files; i++) {
+    if (check_wav_extension(namelist[i]->d_name) == 0) {
+      filename_list = g_list_append(filename_list, namelist[i]->d_name);
+    }
+  }
+
+  return 0;
+}
 
 // Public Functions ///////////////////////////////////////////////////////////
 
-int mp3_encoder_init(void)
+int mp3_encoder_init(char *folder_name)
 {
+  if (get_wav_list(folder_name) < 0) {
+    printf("Unable get wav files\n");
+    return -1;
+  }
+
+#ifdef DEBUG
+  GList *elem;
+  for (elem = g_list_first(filename_list); elem != NULL ; elem = g_list_next(elem)) {
+    printf("%s\n", (char *) elem->data);
+  }
+#endif
+
   return 0;
 }
 
 int mp3_encoder_finish(void)
 {
+
+  g_list_free(filename_list);
+
   return 0;
 }
 
--- src/mp3_encoder_main.c
+++ src/mp3_encoder_main.c
@@ -1,5 +1,19 @@
-// Created by Diogo Justen (diogojusten@gmail.com) in November 2017
-// Copyright (C) Diogo Justen. All rights reserved.
+/*
+ * Copyright (C) 2017  Diogo Justen. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // Includes ///////////////////////////////////////////////////////////////////
 
@@ -9,6 +23,9 @@
 #include "mp3_encoder.h"
 
 // Definitions ////////////////////////////////////////////////////////////////
+
+#define MAX_ARGS 2
+#define MAX_FOLDER_SIZE 255
 
 // Macros /////////////////////////////////////////////////////////////////////
 
@@ -31,7 +48,27 @@ static void usage(void)
 
 int main(int argc, char **argv)
 {
-  usage();
+  char folder_name[MAX_FOLDER_SIZE];
+
+  if (argc != MAX_ARGS) {
+    usage();
+    return -1;
+  }
+
+  if (strncpy(folder_name, argv[1], sizeof(folder_name)) < 0) {
+    printf("Unable get folder name\n");
+    return -1;
+  }
+
+  if (mp3_encoder_init(folder_name) < 0) {
+    printf("Unable init mp3 encoder\n");
+    return -1;
+  }
+
+  if (mp3_encoder_finish() < 0) {
+    printf("Unable finish mp3 encoder\n");
+    return -1;
+  }
 
   return 0;
 }
